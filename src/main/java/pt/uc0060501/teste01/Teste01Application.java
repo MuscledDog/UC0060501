13228aaf6935227955c7882b2f5096657c77b88e
MuscledDog <lemos.vasco@gmail.com>
2026-04-17 21:57:41 +0100

    1 push

---
+++ src/main/java/pt/uc0060501/teste01/Teste01Application.java
@@ -0,0 +1,13 @@
+package pt.uc0060501.teste01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste01Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/uc0060501/teste01/Teste01ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.uc0060501.teste01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teste01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
